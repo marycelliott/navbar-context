b49ce4ac8cc31382f1eaa7547e94d5e9134f7330
Mary Claire Elliott <spitsfire@Marys-MacBook-Pro.local>
2020-03-09 16:23:54 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Context from './context/Context';
 import './App.css';
+import Navbar from './components/Navbar';
+import FormWrapper from './components/FormWrapper';
 
 function App() {
+  const [name,setName] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Context.Provider value={{name,setName}}>
+        <Navbar />
+        <FormWrapper />
+      </Context.Provider>
     </div>
   );
 }
